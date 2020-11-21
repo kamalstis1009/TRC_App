ba36aacb6eeeb8a3eeb1c6aa6083af45920316b8
Mustofa Kamal <himustofa@outlook.com>
2020-11-21 18:37:13 +0600

    Initial commit

--- app/src/main/java/com/trc/app/views/fragments/FragmentApplicationForm.java
+++ app/src/main/java/com/trc/app/views/fragments/FragmentApplicationForm.java
@@ -7,13 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.trc.app.R;
+import com.trc.app.utils.Utility;
+
+import java.util.Objects;
 
 public class FragmentApplicationForm extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_application_form, container, false);
+        View view = inflater.inflate(R.layout.fragment_application_form, container, false);
+
+        ((Button) view.findViewById(R.id.user_submit_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Utility.getInstance().onAddFragment(Objects.requireNonNull(getActivity()).getSupportFragmentManager(), new FragmentHome());
+            }
+        });
+
+        return view;
     }
 }
